2b58f28ad1f5781b70b886429e818d793e82db26
Italo Oliveira <italooliveira-dev@hotmail.com>
2025-06-08 23:45:51 -0300

    feat: Adiciona a implementação do Cloud Gateway

---
+++ mscloudgateway/src/main/java/br/com/italooliveira/mscloudgateway/MscloudgatewayApplication.java
@@ -0,0 +1,25 @@
+package br.com.italooliveira.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MscloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+					.route(rota -> rota.path("/clientes/**").uri("lb://mscliente"))
+				.build();
+	}
+}
